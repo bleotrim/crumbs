81724eedbdc5f45566383d151dc2a0b0f0f00e7c
Leotrim <bleotrim@gmail.com>
2025-08-20 12:29:19 +0200

    Add SimpleLogger class for logging functionality

---
+++ src/Crumbs/SimpleLogger.cs
@@ -0,0 +1,31 @@
+public class SimpleLogger
+{
+    private readonly string _logFilePath;
+    private readonly object _lock = new object();
+
+    public SimpleLogger(string logFilePath)
+    {
+        _logFilePath = logFilePath;
+        //Directory.CreateDirectory(Path.GetDirectoryName(_logFilePath)!);
+    }
+
+    public void Info(string message)
+    {
+        Log("INFO", message);
+    }
+
+    public void Error(string message)
+    {
+        Log("ERROR", message);
+    }
+
+    private void Log(string level, string message)
+    {
+        var logLine = $"{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss.fff} [{level}] {message}";
+        lock (_lock)
+        {
+            Console.WriteLine(logLine);
+            File.AppendAllText(_logFilePath, logLine + Environment.NewLine);
+        }
+    }
+}
