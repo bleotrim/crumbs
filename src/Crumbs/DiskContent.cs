2c025bb7b451a4083813b9c551fcc1686526de4b
Leotrim <bleotrim@gmail.com>
2025-08-20 16:01:17 +0200

    Rename class for managing disk-related information

---
+++ src/Crumbs/DiskContent.cs
@@ -0,0 +1,8 @@
+public class DiskContent
+{
+    public required string SerialNumber { get; set; }
+    public required string PartNumber { get; set; }
+    public long FileCount { get; set; }
+    public long TotalFileSize { get; set; }
+    public required List<FileDetail> FileDetails { get; set; }
+}
